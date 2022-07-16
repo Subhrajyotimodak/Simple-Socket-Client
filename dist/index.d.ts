6793bf03758b396c4921f514d258ab4339baf894
Subhrajyotimodak <writer1ani@gmail.com>
2022-07-16 18:39:25 +0530

    tes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 import * as core from "./core";
 import * as reactProvider from "./react-provider";
-import * as utils from "util";
+import * as utils from "utils";
 export { core, reactProvider, utils };
